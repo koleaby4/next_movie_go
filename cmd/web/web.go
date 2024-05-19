f999eb119fde02999d2695fb933f56f11cfa1bdf
Nicolai Negru <negru.n@gmail.com>
2024-05-19 12:32:14 +0100

    working towards movie_detail

--- cmd/web/web.go
+++ cmd/web/web.go
@@ -1,11 +1,42 @@
 package main
 
 import (
+	"context"
 	"github.com/koleaby4/next_movie_go/config"
+	"github.com/koleaby4/next_movie_go/internal/db"
 	"github.com/koleaby4/next_movie_go/internal/tmdb"
 	"html/template"
 	"net/http"
+	"strings"
 )
+
+func movieDetail(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.Path, "/")
+	movieID := parts[len(parts)-1]
+
+	ctx := context.Background()
+	conn := db.NewConnection("", ctx)
+	defer conn.Close(ctx)
+	queries := db.New(conn)
+
+	movie, err := queries.GetMovie(ctx, movieID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("internal/web/templates/movie_detail.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, movie)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
 
 func mostPopularMovies(w http.ResponseWriter, r *http.Request) {
 	movies, err := tmdb.GetMostPopularMovies(tmdbConfig, 8)
@@ -34,5 +65,6 @@ var tmdbConfig = tmdb.Config{
 
 func main() {
 	http.HandleFunc("/movies/most-popular", mostPopularMovies)
+	http.HandleFunc("/movies/{movie_id}", movieDetail)
 	http.ListenAndServe(":8080", nil)
 }
