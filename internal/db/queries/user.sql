48329ce0b81d14603b239fc7032d3097bb6347f3
Nicolai Negru <negru.n@gmail.com>
2024-06-03 22:08:45 +0100

    amend inserUser sql

--- internal/db/queries/user.sql
+++ internal/db/queries/user.sql
@@ -8,5 +8,5 @@ where email = $1;
 INSERT INTO users (email, auth_token, expiry)
 VALUES ($1, $2, NOW() + INTERVAL '24 hours') ON CONFLICT (email)
 DO
-UPDATE SET auth_token = $2, expires = NOW() + INTERVAL '24 hours'
+UPDATE SET auth_token = $2, expiry = NOW() + INTERVAL '24 hours'
     RETURNING id, email, auth_token, expiry;
--- internal/db/user.sql.go
+++ internal/db/user.sql.go
@@ -31,7 +31,7 @@ const upsertUser = `-- name: UpsertUser :one
 INSERT INTO users (email, auth_token, expiry)
 VALUES ($1, $2, NOW() + INTERVAL '24 hours') ON CONFLICT (email)
 DO
-UPDATE SET auth_token = $2, expires = NOW() + INTERVAL '24 hours'
+UPDATE SET auth_token = $2, expiry = NOW() + INTERVAL '24 hours'
     RETURNING id, email, auth_token, expiry
 `
 
