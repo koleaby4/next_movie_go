bedf691414ec5a7beb5d9568d37b0dd79e4728c7
Nicolai Negru <negru.n@gmail.com>
2024-06-09 18:07:10 +0100

    sql -> UpsertMovieWatchedByUser

--- db/queries/movieswatchedbyuser.sql
+++ db/queries/movieswatchedbyuser.sql
@@ -3,19 +3,14 @@ select *
 from movies_watched_by_user
 where user_id = $1;
 
--- name: AddMovieWatchedByUser :execresult
-insert into movies_watched_by_user (user_id, movie_id, experience_stars)
-values ($1, $2, $3);
+-- name: UpsertMovieWatchedByUser :exec
+INSERT INTO movies_watched_by_user (user_id, movie_id, experience_stars)
+VALUES ($1, $2, $3)
+ON CONFLICT (user_id, movie_id)
+    DO UPDATE SET experience_stars = EXCLUDED.experience_stars;
 
 -- name: RemoveMovieWatchedByUser :exec
 delete
 from movies_watched_by_user
 where user_id = $1
   and movie_id = $2;
-
--- name: UpdateMovieWatchedByUser :exec
-update movies_watched_by_user
-set experience_stars = $1
-where user_id = $2
-  and movie_id = $3;
-
