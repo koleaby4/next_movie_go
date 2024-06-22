b9c64850967ce1c84a2397894d9c821a0eb08db4
Nicolai Negru <negru.n@gmail.com>
2024-06-22 15:57:30 +0100

    add docstrings to handlers.go

--- web/handlers/handlers.go
+++ web/handlers/handlers.go
@@ -4,10 +4,12 @@ import (
 	"github.com/koleaby4/next_movie_go"
 )
 
+// Handlers is the struct for the handlers
 type Handlers struct {
 	AppConfig next_movie_go.AppConfig
 }
 
+// New creates a new Handlers struct
 func New(cfg next_movie_go.AppConfig) *Handlers {
 	return &Handlers{
 		AppConfig: cfg,
