c9021eaa01bbbb487226fff003b914cfb8e3d799
Nicolai Negru <negru.n@gmail.com>
2024-06-29 11:53:48 +0100

    refactor config_test.go

--- config/config_test.go
+++ config/config_test.go
@@ -8,33 +8,30 @@ import (
 )
 
 func TestGetConfig(t *testing.T) {
-	// Set environment variables
-	os.Setenv("DB_USER", "testuser")
-	os.Setenv("POSTGRES_PASSWORD", "testpass")
-	os.Setenv("DB_HOST", "localhost")
-	os.Setenv("DB_NAME", "testdb")
-	os.Setenv("TMDB_API_KEY", "testapikey")
-	os.Setenv("TMDB_BASE_URL", "testbaseurl")
-	os.Setenv("TMDB_BACKLOAD_HIGH_WATERMARK_DATE", "2023-01-28")
+	envars := map[string]string{
+		"DB_USER":                           "testuser",
+		"POSTGRES_PASSWORD":                 "testpass",
+		"DB_HOST":                           "localhost",
+		"DB_NAME":                           "testdb",
+		"TMDB_API_KEY":                      "testapikey",
+		"TMDB_BASE_URL":                     "testbaseurl",
+		"TMDB_BACKLOAD_HIGH_WATERMARK_DATE": "2023-01-28",
+	}
 
-	// Call GetConfig
+	for k, v := range envars {
+		os.Setenv(k, v)
+	}
+
 	cfg, err := GetConfig()
 
-	// Assert no error
 	assert.NoError(t, err)
 
-	// Assert correct values
 	assert.Equal(t, "postgres://testuser:testpass@localhost:5432/testdb?sslmode=disable", cfg.DbDsn)
 	assert.Equal(t, "testapikey", cfg.TmdbConfig.APIKey)
 	assert.Equal(t, "testbaseurl", cfg.TmdbConfig.BaseURL)
 	assert.Equal(t, "2023-01-28", cfg.TmdbConfig.BackloadHighWatermarkDate)
 
-	// Unset environment variables
-	os.Unsetenv("DB_USER")
-	os.Unsetenv("POSTGRES_PASSWORD")
-	os.Unsetenv("DB_HOST")
-	os.Unsetenv("DB_NAME")
-	os.Unsetenv("TMDB_API_KEY")
-	os.Unsetenv("TMDB_BASE_URL")
-	os.Unsetenv("TMDB_BACKLOAD_HIGH_WATERMARK_DATE")
+	for k := range envars {
+		os.Unsetenv(k)
+	}
 }
